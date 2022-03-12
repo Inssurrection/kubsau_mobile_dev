eb57d3a6b16cad018c23fa94023014967fe61df7
inssurrection <kozhara1996@gmail.com>
2022-03-12 22:19:53 +0300

    Lab3 is done

--- lib/main.dart
+++ lib/main.dart
@@ -1,179 +1,132 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MaterialApp(
+    home: HomePanel(),
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class HomePanel extends StatefulWidget {
+  const HomePanel({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.cyan,
-      ),
-      home: const MyHomePage(title: 'Инкремент'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<HomePanel> createState() => _HomePanelState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
+class _HomePanelState extends State<HomePanel> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  int _likeCounter = 0;
+  void likesAmountCounter() {
     setState(() {
-
-      _counter++;
+      _likeCounter++;
     });
   }
-  void _decrementCounter() {
-    setState(() {
-
-      _counter--;
-    });
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(vsync: this);
   }
-  void _incrementWipe(){
-    setState(() {
-      _counter = 0;
-    });
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
-  Widget buildButton () => Container(
-
-      margin:  EdgeInsets.only(left:107,right:107, bottom: 20),
-      decoration: const BoxDecoration(
-        borderRadius: BorderRadius.vertical(top: Radius.circular(32))
-      ),
-      child: Row(
-        mainAxisSize: MainAxisSize.min,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Expanded(
-            flex: 0,
-
-          child: ElevatedButton(
-
-          child: const Text('-', style: TextStyle( color: Colors.black),),
-          onPressed: () => _decrementCounter(),
-          style: ElevatedButton.styleFrom(
-          primary: Colors.red,
-
-
-          textStyle: const TextStyle(
-          fontSize: 25,
-          fontWeight: FontWeight.bold,
-        ),
-     ),
-    ),
-            ),
-          Expanded(
-            flex: 0,
-            child: ElevatedButton(
-              child: Text('+', style: TextStyle( color: Colors.black) ,),
-              onPressed: () => _incrementCounter(),
-              style: ElevatedButton.styleFrom(
-                  primary: Colors.green,
-
-                  textStyle: const TextStyle(
-                    fontSize: 25,
-                    fontWeight: FontWeight.bold,
-
-                  )
-
-              ),
-            ),
-          ),
-
-           ]
-
-          ),
-
-          );
-  Widget bulidText () => Container(
-    child: Text('Значение инкремента:'),
-    margin: EdgeInsets.only(top: 250 ),
-  );
-  Widget buildCounter () => Container(
-    margin: EdgeInsets.only(bottom: 10 ),
-    padding: EdgeInsets.only(top: 0),
-    child: Text('$_counter', style: TextStyle(color: Colors.grey[600], fontSize: 30),),
-    alignment: Alignment.center,
-
-  );
-  Widget bulidWipe () => Container(
-      margin: EdgeInsets.only(bottom: 282 ),
-      child: TextButton(child: Text('Сбросить', style: TextStyle(color: Colors.grey)),
-      onPressed: () =>{_incrementWipe()},style: TextButton.styleFrom(
-      ),),
-
-  );
 
   @override
   Widget build(BuildContext context) {
 
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
-
-        title: Text(widget.title),
+        title: Text('Общежитие №20'),
+        backgroundColor: Colors.green,
       ),
-      body: Center(
+      body: SingleChildScrollView(
+      child: SafeArea(
+        child: Column(
+          children: [
+            Image.asset('assets/images/Hostel_20.jpg'),
+            Padding(padding: EdgeInsets.only(bottom: 20)),
+            Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                      Text('Общежитие №20', style: TextStyle(
+                        fontWeight: FontWeight.bold,
+
+                      )),
+                      Text('Краснодар, ул. Калинина, 13', style: TextStyle(
+                        color: Colors.grey,
+                        fontFamily: 'TimesNewRoman',
+                      ))
+                    ],),
+
+                    Row(
+                      children: [
+                        TextButton.icon(
+                            onPressed: ()=>{likesAmountCounter()},
+                            icon: Icon(Icons.favorite, color: Colors.red,),
+                            label: Text('$_likeCounter'))
+                      ],
+                    )
+                  ],
+                ),
+                SizedBox(width: 100,height: 20,),
+                Row(
+
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
 
 
-       child: Column(
+                    Column(children: [
+                      IconButton(onPressed: ()=>{}, icon: Icon(Icons.phone),color: Colors.green,),
+                      Text('Позвонить', style: TextStyle(color: Colors.green, )),
 
-         children: <Widget>[
-           Expanded(
-              flex:0,
-             child: bulidText(),
-           ),
-           Expanded(
-             flex:0,
-             child: buildCounter(),
-           ),
-           Expanded(
-                flex:0,
-               child: buildButton(),
-           ),
-           Expanded( flex: 0,
-              child: bulidWipe(),
-           ),
-         ],
-       ),
+                    ],),
+                    Column(children: [
+                      IconButton(onPressed: ()=>{}, icon: Icon(Icons.near_me_sharp),color: Colors.green,),
+                      Text('Маршрут', style: TextStyle(color: Colors.green))
+                      ]
+                    ),
+                    Column(children: [
 
+                      IconButton(onPressed: ()=>{}, icon: Icon(Icons.share),color: Colors.green,),
+                      Text('Поделиться', style: TextStyle(color: Colors.green))
+                    ]
+
+                    ),
+
+                  ],
+                ),
+                SizedBox(width: 100,height: 20,),
+                Row(
+                  children: [
+                    Flexible(child: Text('Студенческий городок или так'
+                        ' называемый кампус Кубанского ГАУ состоит'
+                        ' из двадцати общежитий, в которых проживает '
+                        'более 8000 студентов, что составляет 96% от всех'
+                        ' нуждающихся. Студенты первого курса обеспечены'
+                        ' местами в общежитии полностью. В соответствии с'
+                        ' Положением о студенческих общежитиях университета,'
+                        ' при поселении между администрацией и студентами'
+                        ' заключается договор найма жилого помещения.'
+                        ' Воспитательная работа в общежитиях направлена на улучшение быта, соблюдение правил внутреннего распорядка, отсутствия асоциальных явлений в молодежной среде. Условия проживания в общежитиях университетского кампуса полностью отвечают санитарным нормам и требованиям: наличие оборудованных кухонь, душевых комнат, прачечных, читальных залов, комнат самоподготовки, помещений для заседаний студенческих советов и наглядной агитации. С целью улучшения условий быта студентов активно работает система студенческого самоуправления - студенческие советы организуют всю работу по самообслуживанию.'))
+
+                  ],
+                ),
+              ],
+            )
+          ],
+        )
       ),
-
-
-
+      ),
     );
-
   }
 }
+
